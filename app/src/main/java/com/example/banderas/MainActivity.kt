52d096537e3b8b52392bc8880d7e76bff0c2418d
Luis <lagui@gmail.com>
2026-09-24 10:39:51 -0600

    Practica-01-Mexico - Screen (Row + Box + Image)

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.banderas
 
-
+import Screen.BanderaMexico
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.banderas.ui.theme.BanderasTheme
+
 
 class MainActivity : ComponentActivity()
 {
@@ -21,21 +33,20 @@ class MainActivity : ComponentActivity()
         setContent {
             BanderasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Bandera(modifier = Modifier.padding(innerPadding),)
+                    BanderaMexico(modifier = Modifier.padding(innerPadding),)
                 }
             }
         }
     }
 }
 
-fun Bandera(modifier: Modifier = Modifier)
-{
-}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun BanderaPreview() {
+fun BanderaPreview()
+{
     BanderasTheme {
-        Bandera()
+        BanderaMexico()
     }
 }
