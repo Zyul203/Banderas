4a768abcdcae9a1777f4832cb618a65cd4d70b6a
Luis <lagui@gmail.com>
2026-09-24 13:01:59 -0600

    Practica-03-Italia: Box + Column

--- app/src/main/java/Screen/Bandera.kt
+++ app/src/main/java/Screen/Bandera.kt
@@ -1,2 +1,50 @@
 package Screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.banderas.R
+import com.example.banderas.ui.theme.BanderasTheme
+
+@Composable
+fun BanderaItalia(modifier: Modifier = Modifier)
+{
+    Column (modifier = modifier.fillMaxSize())
+    {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.Red)
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(Color.White)
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .background(colorResource(id = R.color.verde_Italia))
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BanderaPreview()
+{
+    BanderasTheme {
+        BanderaItalia()
+    }
+}
--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.banderas
 
 
+import Screen.BanderaItalia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,21 +22,19 @@ class MainActivity : ComponentActivity()
         setContent {
             BanderasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Bandera(modifier = Modifier.padding(innerPadding),)
+                    BanderaItalia(modifier = Modifier.padding(innerPadding),)
                 }
             }
         }
     }
 }
 
-fun Bandera(modifier: Modifier = Modifier)
-{
-}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun BanderaPreview() {
+fun BanderaPreview()
+{
     BanderasTheme {
-        Bandera()
+        BanderaItalia()
     }
 }
